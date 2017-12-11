767d8e3d13fd521e1ac43296a37a2d784537dbe6
Kisho Shiraishi <e175761@ie.u-ryukyu.ac.jp>
2017-12-11 12:08:06 +0900

    Final commit

--- src/main/java/jp/ac/uryukyu/ie/e175761/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e175761/Enemy.java
@@ -33,4 +33,21 @@ public class Enemy extends LivingThing{
             System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
+    public void attack(LivingThing opponent){
+        if(isDead() == false) {
+            int damage = (int) (Math.random() * getAttack());
+            int luck = (int)(Math.random()*10);
+            if(damage <=0) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName(), damage);
+                opponent.wounded(damage);
+            }else if(luck <= 2){
+                damage = damage * 2;
+                System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                opponent.wounded(damage);
+            }else {
+                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                opponent.wounded(damage);
+            }
+        }
+    }
 }
--- src/main/java/jp/ac/uryukyu/ie/e175761/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e175761/Hero.java
@@ -34,4 +34,22 @@ public class Hero extends LivingThing{
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
         }
     }
+    public void attack(LivingThing opponent){
+        if(isDead() == false) {
+            int damage = (int) (Math.random() * getAttack());
+            int luck = (int)(Math.random()*4);
+            if(damage <=0) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName(), damage);
+                opponent.wounded(damage);
+            }else if(luck <= 1){
+                damage = damage * 2;
+                System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                opponent.wounded(damage);
+            }else {
+                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                opponent.wounded(damage);
+            }
+        }
+    }
 }
+
